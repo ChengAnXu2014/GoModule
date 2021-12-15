bc122de27346fc451315266303f2d3577011d7bd
程安絮 <74345648+ChengAnXu2014@users.noreply.github.com>
2021-12-15 14:44:30 +0800

    Create remote.go

---
+++ remote.go
@@ -0,0 +1,3 @@
+package remote  // "hello.go" will refer variable Str with name "remote.Str", the package name "remote" is also used as namespace.
+
+var Str = "I Love Aoi Sola" // Variable/Function name's first letter must be upercase, or they can't be accessed out of current module.
